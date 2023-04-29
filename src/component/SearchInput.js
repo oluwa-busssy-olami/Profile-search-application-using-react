a433df7d45a7a5ee29ba6d15017aafba6761ef67
Busolami <oluwabibusola@gmail.com>
2023-04-30 00:56:14 +0100

    Event-handler-in-SearchInput

--- src/component/SearchInput.js
+++ src/component/SearchInput.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
 import { ProfileData } from "../Data/ProfileData.js";
 
-const SearchInput = ({ setProfileFiltered }) => {
-  const [SearchInput, setSearchInput] = useState("");
+const SearchInput = () => {
+  const [search, setSearch] = useState("");
+
   const handleSearchInput = (event) => {
-    const query = event.target.value;
-    setSearchInput(query);
-    setProfileFiltered(
-      ProfileData.filter((profileData) =>
-        profileData.firstNmae.tolowerCase().includes(query.tolowerCase())
+    setSearch(event.target.value);
+  };
+
+  const profileOject = ["id", "title", "firstName", "lastName"];
+  const inputSearch = ({ ProfileData }) => {
+    return { ProfileData }.filter((profilePerson) =>
+      profileOject.some((profileOjects) =>
+        profileOject[profileOjects].toLowerCase().includes(search.toLowerCase())
       )
     );
   };
+
   return (
     <div className="searchInput">
       <input
         type="text"
-        value={SearchInput}
+        value={search}
         placeholder="search by name..."
-        onChange={{ handleSearchInput }}
+        onChange={handleSearchInput}
       />{" "}
     </div>
   );
